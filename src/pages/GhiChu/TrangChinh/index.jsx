a4ec1bcac46d4602805558f8d2a5918430a7669f
vandat4523 <tranvandat452k3@gmail.com>
2024-05-19 23:19:33 +0700

    trang chinh up lai

--- src/pages/GhiChu/TrangChinh/index.jsx
+++ src/pages/GhiChu/TrangChinh/index.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import style from "./TrangChinh.module.css";
+import { IoAddCircleOutline } from "react-icons/io5";
 
 const initialNotes = [
   {
     tieude: "Note 1",
-    nd: "chua dVăn học thành văn chịu ảnh hưởng của văn học dân gian về nhiều phương diện, từ nội dung tư tưởng đến hình thức nghệ thuật. Văn học viết cũng có tác động trở lại đối với văn học dân gian trên một số phương diện. Mối quan hệ giữa văn học dân gian với văn học viết cũng như vai trò, ảnh hưởng của văn học dân gian đối với văn học thể hiện trọn vẹn hơn cả ở lĩnh vực sáng tác và ở bộ phận thơ văn quốc âm.ien ",
+    nd: "Văn học thành văn chịu ảnh hưởng của văn học dân gian về nhiều phương diện, từ nội dung tư tưởng đến hình thức nghệ thuật. Văn học viết cũng có tác động trở lại đối với văn học dân gian trên một số phương diện. Mối quan hệ giữa văn học dân gian với văn học viết cũng như vai trò, ảnh hưởng của văn học dân gian đối với văn học thể hiện trọn vẹn hơn cả ở lĩnh vực sáng tác và ở bộ phận thơ văn quốc âm.",
   },
   {
     tieude: "Note 2",
@@ -34,23 +35,17 @@ export default function TrangChinh() {
       <div className={style.document}>
         <div className={style.tieudeto}>
           <h3 className={style.tieude}>Ghi Chú Của Bạn</h3>
-          <img
-            src="https://png.pngtree.com/png-vector/20191018/ourmid/pngtree-plus-icon-png-image_1826455.jpg"
-            alt="plus-icon"
-            className={style.img}
-            onClick={addNewNote}
-            style={{ cursor: "pointer" }}
-          />
+          <IoAddCircleOutline className={style.img} onClick={addNewNote} />
         </div>
         <br />
         <div className={style.thenote}>
           {notes.map((note, index) => (
             <div key={index} className={style.card}>
+              <div className={style.noteIndex}>{index + 1}</div>
               <h2 onClick={() => setSelectedNote(note)}>{note.tieude}</h2>
               <p onClick={() => setSelectedNote(note)}>
                 {note.nd.length > 35 ? note.nd.slice(0, 35) + "..." : note.nd}
               </p>
-              
             </div>
           ))}
         </div>
